947d9a6d8443d31aacfc97603889e66b18795b7f
Тимур <96944503+timur-mir@users.noreply.github.com>
2026-07-04 16:26:42 +0500

    Update cacheservice.dart

--- lib/utils/cacheservice.dart
+++ lib/utils/cacheservice.dart
@@ -8,13 +8,11 @@ import '../models/nasaimage.dart';
 class CacheService {
   static const String _cacheKey = 'nasa_apod_cache';
 
-  // Сохранение данных в кэш
   Future<void> saveNasaData(NasaImage data) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_cacheKey, jsonEncode(data.toJson()));
   }
 
-  // Получение данных из кэша
   Future<NasaImage?> getCachedNasaData() async {
     final prefs = await SharedPreferences.getInstance();
     final jsonString = prefs.getString(_cacheKey);
@@ -24,7 +22,6 @@ class CacheService {
     return NasaImage.fromJson(json);
   }
 
-  // Очистка кэша
   Future<void> clearCache() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_cacheKey);
